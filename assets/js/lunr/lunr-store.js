5df802eb61fac2bb10e4f5ae1123830af2a8d9a7
Moutella <Moutella@users.noreply.github.com>
2022-02-09 01:54:45 +0000

    jekyll build from Action 8166586d95aa6a6e3ddf4e5aed807a12983d195e

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Better coder, better me",
+        "excerpt":"A few months after graduating, I have realized that I don’t really know a lot of stuff that I should, and that there are thousands of improvementes that I can work on. Because of that, I’ve decided that it is time to start re-learning a lot of basic computer science,...","categories": ["objectives"],
+        "tags": [],
+        "url": "/objectives/2022/02/08/my-first-post.html",
+        "teaser": null
+      },{
+        "title": "Just take it a day at a time 🙂",
+        "excerpt":"I just saw the first video of the coding-interview-university, where a Harvard student explains basic asymptotic notation, like what does O(n), O(1), O(n^2) means and so on, which is kind of a breeze for anyone who has taken a few computer science classes. However, I do admit that I had...","categories": ["objectives","learning","coding-interview-university"],
+        "tags": [],
+        "url": "/objectives/learning/coding-interview-university/2022/02/09/first-day.html",
+        "teaser": null
+      }]
